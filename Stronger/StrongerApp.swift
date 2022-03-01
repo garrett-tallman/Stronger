9259b4d0978b340e3f6e5e983b8beee11f78a01d
Garrett Tallman <gbtallman@icloud.com>
2022-03-01 11:12:27 -0600

    Initial Commit

---
+++ Stronger/StrongerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrongerApp.swift
+//  Stronger
+//
+//  Created by Garrett Tallman on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct StrongerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
